ffb43b8c83ee5e0e9290141836a50ef7cf0c05a3
Eliseo Falcon <eliseonop@gmail.com>
2022-07-13 09:06:17 -0500

    update

--- Program.cs
+++ Program.cs
@@ -5,3 +5,8 @@ string nombre = Console.ReadLine();
 
 
 Console.WriteLine("Hola " + nombre);
+
+Console.WriteLine("Dime la hora");
+string hora = Console.ReadLine();
+
+Console.WriteLine("la hora es " + hora);
